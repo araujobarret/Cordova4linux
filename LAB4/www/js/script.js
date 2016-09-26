497119724d016a83cd12a7eedb21a12a290c4236
Carlos <tiamatpontal@gmail.com>
2016-09-26 13:33:48 -0300

    Lab 04 - task 4

--- LAB4/www/js/script.js
+++ LAB4/www/js/script.js
@@ -9,16 +9,28 @@ var app = {
 	},
 	onDeviceReady: function(){
 		app.receivedEvent();
+		app.onDeviceInfo();
 	},
 	receivedEvent: function(){
 		var pronto = document.getElementById("secundaria").querySelector("#ready");
+		
 		pronto.innerHTML = "Carregado!";
+
+		console.log(device);
 	},
 	onDevicePause: function(){
 		alert("App enviada para background.");
 	},
 	onDeviceResume: function(){
 		alert("App trazida do background.");
+	},
+	onDeviceInfo: function(){
+		var dispositivo = document.getElementById("dispositivo").querySelector("#info");
+		dispositivo.innerHTML = "Cordova: " + device.cordova;
+		dispositivo.innerHTML += "<br/>Platform: " + device.platform;
+		dispositivo.innerHTML += "<br/>UUID: " + device.uuid;
+		dispositivo.innerHTML += "<br/>Versao: " + device.version;
+		dispositivo.innerHTML += "<br/>Modelo: " + device.model;
 	}
 }
 
