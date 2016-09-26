c48e4a93d0bc25929c95dcae43abbe7c10a97e4c
Carlos <tiamatpontal@gmail.com>
2016-09-26 12:38:03 -0300

    Lab 04 - task 3

--- LAB4/www/js/script.js
+++ LAB4/www/js/script.js
@@ -4,14 +4,21 @@ var app = {
 	},
 	deviceReady: function(){
 		document.addEventListener("deviceready", this.onDeviceReady, false);
+		document.addEventListener("pause", this.onDevicePause ,false);
+		document.addEventListener("resume", this.onDeviceResume ,false);
 	},
 	onDeviceReady: function(){
 		app.receivedEvent();
 	},
 	receivedEvent: function(){
 		var pronto = document.getElementById("secundaria").querySelector("#ready");
-
 		pronto.innerHTML = "Carregado!";
+	},
+	onDevicePause: function(){
+		alert("App enviada para background.");
+	},
+	onDeviceResume: function(){
+		alert("App trazida do background.");
 	}
 }
 
